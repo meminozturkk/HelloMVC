81aa50989e41a72d10a4ba09b91d5edc637e8492
meminozturkk <180101051m@gmail.com>
2023-07-18 17:04:49 +0300

    Add project files.

---
+++ HelloMVC/Controllers/HelloController.cs
@@ -0,0 +1,43 @@
+﻿using HelloMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloMVC.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: Hello
+        public ActionResult Index()
+        {
+
+            return View();
+           
+		}
+		//penultimate codes not deleted
+		public ActionResult List(/*string searchToken*/)
+        {
+            List<Car> cars = new List<Car>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Car car = new Car();
+                car.Brand = "BMW";
+                car.LicensePlate = $"{i}";
+                cars.Add(car);
+            }
+
+			//string encoded = HttpUtility.HtmlEncode(searchToken);
+   //         ViewBag.DisplayMessage = encoded;
+			return View(cars);
+        }
+        public string ShowDetail(string id)
+        {
+			string encodedToken = HttpUtility.HtmlEncode(id);
+			return "Record to Show:" + id;
+        }
+
+	}
+}
---
+++ HelloMVC/Models/Car.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HelloMVC.Models
+{
+	public class Car
+	{
+		public string Brand { get; set; }
+		public string LicensePlate { get; set; }
+	}
+}
